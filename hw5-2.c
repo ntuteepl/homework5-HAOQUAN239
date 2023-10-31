a65e735814bfdc745882b93142b5b4e2f6115d68
HAOQUAN239 <146065167+HAOQUAN239@users.noreply.github.com>
2023-10-31 21:27:03 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int n;
+   double m;
+   scanf("%d",&n);
+   if(n<=800)
+   {
+    m=0.9*n;
+   }
+   if(n>800&&n<1500)
+   {
+    m=n*0.9*0.9;
+   }
+   if(n>=1500)
+   {
+    m=n*0.9*0.79;
+   }
+   printf("%0.1f",m);
+}
 
