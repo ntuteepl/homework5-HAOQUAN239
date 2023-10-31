160e340e14c70280a3f93fbd1b03607b2d273a76
HAOQUAN239 <146065167+HAOQUAN239@users.noreply.github.com>
2023-10-31 21:27:37 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int H,M;
+   double angle,hd,md;
+   scanf("%d%d",&H,&M);
+   hd=H*30+M*0.5;
+   md=6*M;
+   if(hd>md)
+   {
+    angle=hd-md;
+   }
+   else
+   {
+    angle=md-hd;
+   }
+   if(angle>180)
+   {
+       angle=360-angle;
+   }
+   printf("%.3f",angle);
+}
 
