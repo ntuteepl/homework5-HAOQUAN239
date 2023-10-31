49d544100f3a5ffded9fef6030e3fc167955d974
HAOQUAN239 <146065167+HAOQUAN239@users.noreply.github.com>
2023-10-31 21:26:40 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a,h;
+    double m;
+    scanf("%d%d",&h,&a);
+    if(h<=60)
+        {
+            m=a*h;
+        }
+        if(h>60&&h<=120)
+            {
+                 m=a*(h-60)*1.33+a*60;
+            }
+            if(h>120)
+                {
+                    m=a*(h-120)*1.66+a*60*1.33+a*60;
+                }
+                printf("%0.1f",m);
+                }
+
 
